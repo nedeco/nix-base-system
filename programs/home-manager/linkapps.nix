4e25435938fc9536f267ad4fa304f67e03b94097
Daniel Kempkens <daniel+git@kempkens.io>
2025-04-28 16:30:05 +0200

    fix(linkapps): move disablesModules to the top of the attrset

--- programs/home-manager/linkapps.nix
+++ programs/home-manager/linkapps.nix
@@ -13,37 +13,41 @@ in
     enable = lib.mkEnableOption "nedeco.linkapps";
   };
 
+  disabledModules = lib.optionals cfg.enable [ "targets/darwin/linkapps.nix" ];
+
   config = lib.mkIf cfg.enable {
-    disabledModules = [ "targets/darwin/linkapps.nix" ];
+    home = rec {
+      packages = [ ];
 
-    home.activation.aliasApplications =
-      let
-        apps = pkgs.buildEnv {
-          name = "home-manager-applications";
-          paths = config.home.packages;
-          pathsToLink = "/Applications";
-        };
+      activation.aliasApplications =
+        let
+          apps = pkgs.buildEnv {
+            name = "home-manager-applications";
+            paths = packages;
+            pathsToLink = "/Applications";
+          };
 
-        lastAppsFile = "${config.xdg.stateHome}/nix/.apps";
-      in
-      lib.hm.dag.entryAfter [ "writeBoundary" ] # bash
-        ''
-          last_apps=$(cat "${lastAppsFile}" 2>/dev/null || echo "")
-          next_apps=$(readlink -f ${apps}/Applications/* | sort)
+          lastAppsFile = "${config.xdg.stateHome}/nix/.apps";
+        in
+        lib.hm.dag.entryAfter [ "writeBoundary" ] # bash
+          ''
+            last_apps=$(cat "${lastAppsFile}" 2>/dev/null || echo "")
+            next_apps=$(readlink -f ${apps}/Applications/* | sort)
 
-          if [ "$last_apps" != "$next_apps" ]; then
-            echo "Apps have changed. Updating macOS aliases..."
+            if [ "$last_apps" != "$next_apps" ]; then
+              echo "Apps have changed. Updating macOS aliases..."
 
-            apps_path="$HOME/Applications/Home Manager Apps"
-            $DRY_RUN_CMD mkdir -p "$apps_path"
+              apps_path="$HOME/Applications/Home Manager Apps"
+              $DRY_RUN_CMD mkdir -p "$apps_path"
 
-            $DRY_RUN_CMD ${lib.getExe pkgs.fd} \
-              -t l -d 1 . ${apps}/Applications \
-              -x $DRY_RUN_CMD "${pkgs.mkalias}/bin/mkalias" \
-              -L {} "$apps_path/{/}"
+              $DRY_RUN_CMD ${lib.getExe pkgs.fd} \
+                -t l -d 1 . ${apps}/Applications \
+                -x $DRY_RUN_CMD "${pkgs.mkalias}/bin/mkalias" \
+                -L {} "$apps_path/{/}"
 
-            [ -z "$DRY_RUN_CMD" ] && echo "$next_apps" > "${lastAppsFile}"
-          fi
-        '';
+              [ -z "$DRY_RUN_CMD" ] && echo "$next_apps" > "${lastAppsFile}"
+            fi
+          '';
+    };
   };
 }
